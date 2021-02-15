387607240def018ac9b56fee70f33ab618386042
bogdaniordan <bogdan.iordan47@yahoo.com>
2021-02-15 11:52:51 +0200

    Index page redirects to questions_page by using the criteria and direction parameters

--- server.py
+++ server.py
@@ -11,13 +11,13 @@ app.config['UPLOAD_FOLDER'] = '/home/bogdan/Desktop/web projects/ask-mate-1-pyth
 
 @app.route("/")
 def hello():
-    return '<a href="/list/order_by=title&order_direction=asc">List stories</a>'
+    return redirect(url_for('questions_page', criteria='id', direction='asc'))
 
 
 @app.route('/list/order_by=<criteria>&order_direction=<direction>', methods=['GET', 'POST'])
 def questions_page(criteria, direction):
     raw_data = data_manager.read_from_csv(data_manager.questions_file)
-    data = util.sort_data(raw_data, str(criteria), str(direction))
+    sorted_data = util.sort_data(raw_data, criteria, direction)
     if request.method == 'POST':
         img = request.files['img']
         img.save(os.path.join(app.config['UPLOAD_FOLDER'], img.filename))
@@ -26,7 +26,7 @@ def questions_page(criteria, direction):
                     'message': request.form['message']}
         data_manager.append_to_csv(data_row, data_manager.QUESTION_HEADER, data_manager.questions_file)
         return redirect(url_for('individual_q_and_a', question_id=data_row.get('id')))
-    return render_template('list_questions.html', data=data)
+    return render_template('list_questions.html', data=sorted_data)
 
 
 @app.route('/question/<question_id>')
