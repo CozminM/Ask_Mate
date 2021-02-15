85393401506cea0dca77d924f59f44c180db5b3d
bogdaniordan <bogdan.iordan47@yahoo.com>
2021-02-15 13:55:02 +0200

    Added separate route for adding a new question story

--- server.py
+++ server.py
@@ -14,19 +14,10 @@ def index_page():
     return redirect(url_for('questions_page', criteria='id', direction='asc'))
 
 
-#or use uuid for id
 @app.route('/list/order_by=<criteria>&order_direction=<direction>', methods=['GET', 'POST'])
 def questions_page(criteria, direction):
     raw_data = data_manager.read_from_csv(data_manager.questions_file)
     sorted_data = util.sort_data(raw_data, criteria, direction)
-    if request.method == 'POST':
-        img = request.files['img']
-        img.save(os.path.join(app.config['UPLOAD_FOLDER'], img.filename))
-        data_row = {'id': int(raw_data[-1].get('id')) + 1, 'submission_time': int(round(time.time())), 'view_number': 0,
-                    'vote_number': 0, 'title': request.form['title'], 'image': img.filename,
-                    'message': request.form['message']}
-        data_manager.append_to_csv(data_row, data_manager.QUESTION_HEADER, data_manager.questions_file)
-        return redirect(url_for('individual_q_and_a', question_id=data_row.get('id')))
     return render_template('list_questions.html', data=sorted_data)
 
 
@@ -35,6 +26,21 @@ def individual_q_and_a(question_id):
     question = data_manager.get_question_or_answer('id', data_manager.questions_file, question_id)
     answer = data_manager.get_question_or_answer('question_id', data_manager.answers_file, question_id)
     return render_template('individual_question_and_answer_page.html', questions=question, answers=answer)
+
+
+@app.route('/add-question', methods=['GET', 'POST'])
+def add_question_page():
+    raw_data = data_manager.read_from_csv(data_manager.questions_file)
+    if request.method == 'POST':
+        new_id = int(raw_data[-1].get('id')) + 1
+        img = request.files['img']
+        img.save(os.path.join(app.config['UPLOAD_FOLDER'], img.filename))
+        data_row = {'id': new_id, 'submission_time': int(round(time.time())), 'view_number': 0,
+                    'vote_number': 0, 'title': request.form['title'], 'image': img.filename,
+                    'message': request.form['message']}
+        data_manager.append_to_csv(data_row, data_manager.QUESTION_HEADER, data_manager.questions_file)
+        return redirect(url_for('individual_q_and_a', question_id=new_id))
+    return render_template('add_question.html')
 
 
 if __name__ == "__main__":
