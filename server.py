d5dc7480f523fb1b8a2905b62677b49f793cada5
bogdaniordan <bogdan.iordan47@yahoo.com>
2021-03-04 12:52:34 +0200

    Fixed main page id bug

--- server.py
+++ server.py
@@ -11,7 +11,6 @@ app.config['UPLOAD_FOLDER'] = 'static/images/'
 @app.route("/", methods=['GET', 'POST'])
 def index_page():
     first_questions = data_manager.get_questions_by_time()
-    print(first_questions)
     return render_template('main.html', data=first_questions)
 
 
